e477601c1126cf34acacc7ca3a2dd6579235fd08
Chloé Moulin <chloe.moulin@iknsa.com>
2018-08-07 14:44:52 +0200

    filters product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use App\Entity\Material;
 use App\Entity\Product;
 use App\Entity\Shape;
 use App\Entity\Size;
+use Doctrine\ORM\QueryBuilder;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,13 +19,30 @@ class ProductController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $products = $this->getDoctrine()->getManager()->getRepository(Product::class)->findAll();
+        $qb = $this->getDoctrine()->getEntityManager()->createQueryBuilder()->select('p')->from(Product::class, 'p');
+        if($request->request->get('material') && $request->request->get('material') !== 'All')
+        {
+            $qb->join('p.materials', 'm')->where('m.id = :material')->setParameter('material', $request->request->get('material'));
+        }
+        if($request->request->get('shape') && $request->request->get('shape') !== 'All')
+        {
+            $qb->join('p.shapes', 'sh')->andWhere('sh.id = :shape')->setParameter('shape', $request->request->get('shape'));
+        }
+        if($request->request->get('color') && $request->request->get('color') !== 'All')
+        {
+            $qb->join('p.colors', 'c')->andWhere('c.id = :color')->setParameter('color', $request->request->get('color'));
+        }
+        if($request->request->get('size') && $request->request->get('size') !== 'All')
+        {
+            $qb->join('p.sizes', 'si')->andWhere('si.id = :size')->setParameter('size', $request->request->get('size'));
+        }
 
+        $products = $qb->getQuery()->getResult();
         $materials = $this->getDoctrine()->getManager()->getRepository(Material::class)->findAll();
-        //die(var_dump($request));
         $shapes = $this->getDoctrine()->getManager()->getRepository(Shape::class)->findAll();
         $colors = $this->getDoctrine()->getManager()->getRepository(Color::class)->findAll();
         $sizes = $this->getDoctrine()->getManager()->getRepository(Size::class)->findAll();
+        dump($request->request);
 
         return $this->render('products.html.twig', array(
             'products' => $products,
