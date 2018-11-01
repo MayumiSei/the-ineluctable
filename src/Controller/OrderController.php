37a6d0d7a89ec4231aeb23dddba3022514e217d0
Chloé Moulin <chloe.moulin@iknsa.com>
2018-11-01 17:27:41 +0100

    order detail

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,11 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class OrderController extends Controller
 {
+    /**
+     * @Route("/profile/order/{orderNumber}", name="order")
+     */
+    public function indexAction(Request $request) {
+        $order = $this->getDoctrine()->getManager()->getRepository(Order::class)->findOneBy(array(
+            'uniqId' => $request->get('orderNumber')));
 
+        return $this->render('shop/order-detail.html.twig', array(
+            'order' => $order
+        ));
+    }
 }
